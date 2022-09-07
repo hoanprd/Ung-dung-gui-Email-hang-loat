5698dc89068a2cc9522c3b7255ba26a1d48b9b18
hoanprd <99530243+hoanprd@users.noreply.github.com>
2022-09-07 15:09:18 +0700

    CF 07.09.2022 P1

---
+++ SendGmailMulti/SendGmailMulti/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace SendGmailMulti
+{
+    partial class SendGmailMulti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SendGmailMulti));
+            this.AttachTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReceiverTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SubjectTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AttachButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.MailListButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AttachTextBox
+            // 
+            this.AttachTextBox.Location = new System.Drawing.Point(12, 12);
+            this.AttachTextBox.Name = "AttachTextBox";
+            this.AttachTextBox.Size = new System.Drawing.Size(343, 22);
+            this.AttachTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Receiver:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ReceiverTextBox
+            // 
+            this.ReceiverTextBox.Location = new System.Drawing.Point(117, 63);
+            this.ReceiverTextBox.Name = "ReceiverTextBox";
+            this.ReceiverTextBox.ReadOnly = true;
+            this.ReceiverTextBox.Size = new System.Drawing.Size(369, 22);
+            this.ReceiverTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Subject:";
+            // 
+            // SubjectTextBox
+            // 
+            this.SubjectTextBox.Location = new System.Drawing.Point(117, 125);
+            this.SubjectTextBox.Name = "SubjectTextBox";
+            this.SubjectTextBox.Size = new System.Drawing.Size(369, 22);
+            this.SubjectTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(117, 191);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(369, 22);
+            this.UserNameTextBox.TabIndex = 7;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(117, 253);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(369, 22);
+            this.PasswordTextBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Message:";
+            // 
+            // AttachButton
+            // 
+            this.AttachButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AttachButton.Location = new System.Drawing.Point(387, 11);
+            this.AttachButton.Name = "AttachButton";
+            this.AttachButton.Size = new System.Drawing.Size(75, 23);
+            this.AttachButton.TabIndex = 10;
+            this.AttachButton.Text = "Attach";
+            this.AttachButton.UseVisualStyleBackColor = true;
+            this.AttachButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendButton.Location = new System.Drawing.Point(674, 495);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(100, 30);
+            this.SendButton.TabIndex = 11;
+            this.SendButton.Text = "SEND";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(16, 336);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(758, 153);
+            this.MessageTextBox.TabIndex = 12;
+            // 
+            // MailListButton
+            // 
+            this.MailListButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MailListButton.Location = new System.Drawing.Point(503, 63);
+            this.MailListButton.Name = "MailListButton";
+            this.MailListButton.Size = new System.Drawing.Size(75, 23);
+            this.MailListButton.TabIndex = 13;
+            this.MailListButton.Text = "Mail list";
+            this.MailListButton.UseVisualStyleBackColor = true;
+            this.MailListButton.Click += new System.EventHandler(this.MailListButton_Click);
+            // 
+            // SendGmailMulti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 527);
+            this.Controls.Add(this.MailListButton);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.AttachButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UserNameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SubjectTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ReceiverTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AttachTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SendGmailMulti";
+            this.Text = "Send gmail multi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AttachTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ReceiverTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SubjectTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button AttachButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.Button MailListButton;
+    }
+}
+
---
+++ SendGmailMulti/SendGmailMulti/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendGmailMulti
+{
+    public partial class SendGmailMulti : Form
+    {
+        Attachment attach = null;
+        public SendGmailMulti()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                AttachTextBox.Text = dialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() =>
+            {
+                attach = null;
+                try
+                {
+                    FileInfo file = new FileInfo(AttachTextBox.Text);
+                    attach = new Attachment(AttachTextBox.Text);
+                }
+                catch
+                {
+
+                }
+
+                StreamReader sr = new StreamReader(ReceiverTextBox.Text);
+                string email;
+
+                while ((email = sr.ReadLine()) != null)
+                {
+                    GuiMail(UserNameTextBox.Text, email, SubjectTextBox.Text, MessageTextBox.Text, attach);
+                }
+                sr.Close();
+            }
+            );
+            thread.Start();
+        }
+
+        //Hàm gửi mail
+        void GuiMail(string from, string to, string subject, string message, Attachment file = null)
+        {
+            MailMessage mess = new MailMessage(from, to, subject, message);
+
+            if (attach != null)
+            {
+                mess.Attachments.Add(attach);
+            }
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential(UserNameTextBox.Text, PasswordTextBox.Text);
+
+            client.Send(mess);
+        }
+
+        private void MailListButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ReceiverTextBox.Text = dialog.FileName;
+            }
+        }
+    }
+}
